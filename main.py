a5dc0b9cd42b32ca731911e0db6e5243922d6f5c
Thévenin, Vincent <vincent.thevenin@accenture.com>
2024-08-23 13:52:35 +0200

    Add linear estimation

--- main.py
+++ main.py
@@ -61,21 +61,28 @@ def main():
 
             # predicting time left from start to now
             estimate, _, _, _, rcond = np.polyfit(n_list, cum_time_list, 2, full=True)
+            linear_estimate = np.polyfit(n_list, cum_time_list, 1, full=False)
 
             b = -estimate[0] * 2
             a = -estimate[1]
             C = estimate[2]
             formula_C = a*n_list[0] + b/2 * n_list[0]**2
 
+            a_linear = -linear_estimate[0]
+            c_linear = linear_estimate[1]
+
             print("Found: b = {}, a = {}, C = {}, formula_C = {}".format(b, a, C, formula_C))
+            print("Linear estimate: a = {}, c = {}".format(a_linear, c_linear))
             print("Rcond: {}".format(rcond))
 
             # time left
             time_left = a*n + b/2 * n**2
+            linear_time_left = a_linear*n + c_linear
             print("Time left: {} min, {} sec".format(int(time_left//60), int(time_left%60)))
+            print("Linear time left: {} min, {} sec".format(int(linear_time_left//60), int(linear_time_left%60)))
 
-        print("n_list: {}".format(n_list))
-        print("cum_time_list: {}".format(cum_time_list))
+        print("n_list: {}".format(n_list[-8:]))
+        print("cum_time_list: {}".format(cum_time_list[-8:]))
 
         print()
 
